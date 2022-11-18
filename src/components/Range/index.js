98ee70259b491f8dd0eb64b0b974bf93ad52c749
neelam <nishadneelam997@gmail.com>
2022-11-19 01:25:45 +0530

    map function in 2nd widget

--- src/components/Range/index.js
+++ src/components/Range/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./range.css";
 
+let dataArray = ["top", "first", "second", "third", "fourth"];
+
 const Range = () => {
   const [number, setNumber] = useState(0);
   return (
@@ -8,31 +10,17 @@ const Range = () => {
       <p className="number-display">{number + 1}</p>
       <div className="range-inner-container">
         <div class="wifi-symbol">
-          <div
-            class="wifi-circle top"
-            style={{ borderColor: number >= 4 && "#ffffff" }}
-            onClick={() => setNumber(4)}
-          />
-          <div
-            class="wifi-circle first"
-            style={{ borderColor: number >= 3 && "#ffffff" }}
-            onClick={() => setNumber(3)}
-          />
-          <div
-            class="wifi-circle second"
-            style={{ borderColor: number >= 2 && "#ffffff" }}
-            onClick={() => setNumber(2)}
-          />
-          <div
-            class="wifi-circle third"
-            style={{ borderColor: number >= 1 && "#ffffff" }}
-            onClick={() => setNumber(1)}
-          />
-          <div
-            class="wifi-circle fourth"
-            style={{ borderColor: number >= 0 && "#ffffff" }}
-            onClick={() => setNumber(0)}
-          />
+          {dataArray.map((item, index) => {
+            return (
+              <div
+                class={`wifi-circle ${item}`}
+                style={{
+                  borderColor: number >= 4 - index && "#ffffff",
+                }}
+                onClick={() => setNumber(4 - index)}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
